16c178118fd11343f26ff40224f94912e378521b
unknown <mariaecardosocruz@gmail.com>
2026-09-24 22:25:29 -0300

    Feito a questão C e a questão F (ainda vou modificar)

--- funcoes.py
+++ funcoes.py
@@ -5,7 +5,12 @@
 
 
 # ==========-----=========== NOTIFICAÇÕES DAS ESTAÇÕES (QUESTÃO C) ==========-----==========
+def analisar_limites_descarte (lista_descartes, limite_alerta, limite_tolerancia):
+    for i in range(len(lista_descartes)):
+        quantidade = lista_descartes[i]
 
+        if quantidade > limite_tolerancia:
+            print(f"Alerta! No dia {i + 1}, o descarte de {quantidade}kg excedeu a tolerância operacional de {limite_tolerancia}kg!")
 
 # ==========-----========== (QUESTÃO D) ==========-----==========
 
@@ -13,6 +18,13 @@
 
 
 # ==========-----========== REGISTROS DE DESEMPENHO DAS ESTAÇÕES (QUESTÃO F) ==========-----==========
+def filtrar_estacoes_por_eficiencia(base_dados, criterio_corte):
+    estacoes_aprovadas = {}
 
+    for codigo, indice in base_dados.items():
+        if indice >= criterio_corte:
+            estacoes_aprovadas[codigo] = indice
+
+    return estacoes_aprovadas
 
 # ==========-----========== MENU INTERATIVO (QUESTÃO G) ==========-----==========
